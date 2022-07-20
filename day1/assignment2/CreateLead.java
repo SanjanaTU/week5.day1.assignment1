568ab293d88e434bf21eb740be1ec3115552d97d
SanjanaTU <107304167+SanjanaTU@users.noreply.github.com>
2022-07-20 08:36:47 +0530

    Add files via upload

---
+++ day1/assignment2/CreateLead.java
@@ -0,0 +1,29 @@
+package week5.day1.assignment2;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class CreateLead extends BaseClass{
+@Test(invocationCount=1,threadPoolSize=2,invocationTimeOut=1000,priority=1)
+	public void runCreateLead() throws InterruptedException {
+		
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("TestLeaf");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Hari");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("R");
+		driver.findElement(By.id("createLeadForm_primaryPhoneNumber")).sendKeys("99");
+		driver.findElement(By.name("submitButton")).click();
+		driver.close();
+		
+}
+}
+
+
+
+
+
